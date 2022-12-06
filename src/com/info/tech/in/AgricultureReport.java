1591eb8d12ae9c69605773f373159c34d518f708
unknown <chandu.chowdary622@gmail.com>
2022-12-06 14:22:06 +0530

    this is my frist commit

---
+++ src/com/info/tech/in/AgricultureReport.java
@@ -0,0 +1,10 @@
+package com.info.tech.in;
+
+public class AgricultureReport {
+
+	private static String name="It is agriculture report";
+	
+	public void rabbi() {
+		System.out.println("I am in rabbi paddy");
+	}
+}
---
+++ src/com/info/tech/in/WhatherReport.java
@@ -0,0 +1,10 @@
+package com.info.tech.in;
+
+public class WhatherReport {
+
+	private static String name = "it is agriculture report";
+
+	public void rainny() {
+		System.out.println("i am in rainnt seasson");
+	}
+}
